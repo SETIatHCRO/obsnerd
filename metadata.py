796309c4602984031358eee1bc1bc75578e48370
DAVID DEBOER <ddeboer@berkeley.edu>
2023-12-22 12:36:01 -0800

    add logging

--- metadata.py
+++ metadata.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
+
 def start(samp_rate, fn='metadata.yaml'):
     with open(fn, 'a') as fp:
         print(f"tstart: {datetime.now()}", file=fp)
         print(f"bw: {samp_rate / 1E6}", file=fp)
+    with open('onlog.log', 'a') as fp:
+        print(f"{datetime.now()} -- tstart", file=fp)
+        print(f"{datetime.now()} -- bw: {samp_rate}", file=fp)
 
-def stop():
+
+def stop(fn='metadata.yaml'):
     with open(fn, 'a') as fp:
         print(f"tstop: {datetime.now()}", file=fp)
+    with open('onlog.log', 'a') as fp:
+        print(f"{datetime.now()} -- tstop", file=fp)
--- obsnerd.py
+++ obsnerd.py
@@ -2,12 +2,15 @@
 from ATATools import ata_control
 import argparse
 import atexit
+from datetime import datetime
+
 
 ap = argparse.ArgumentParser()
-ap.add_argument('cmd', help="Action [start, move, freq, end)", choices=['start', 'freq', 'move', 'end'], nargs='?', default='start')
+ap.add_argument('cmd', help="Action [start, move, freq, end, note]", choices=['start', 'freq', 'move', 'end', 'note'])
 ap.add_argument('--az', help="Azimuth in deg", type=float, default=121.958)
 ap.add_argument('--el', help="Elevation in deg", type=float, default=23.603)
 ap.add_argument('-f', '--freq', help="Frequency in MHz", type=float, default=1680.0)
+ap.add_argument('-n', '--note', help="Note to add", default=None)
 args = ap.parse_args()
 
 ants = ['1a', '1f', '5c']  # List of antennas that we want to use (the USRPs can use 2 antennas at a time only)
@@ -16,11 +19,15 @@ ants = ['1a', '1f', '5c']  # List of antennas that we want to use (the USRPs can
 # cfreq = 1680
 # az, el = (121.958, 23.603)
 
+fplog = open('onlog.log', 'a')
+
 if args.cmd == 'start':
     ata_control.move_ant_group(ants, 'none', 'atagr')
+    print(f"{datetime.now()} -- start {', '.join(ants)}", file=fplog)
 elif args.cmd == 'end':
     atexit.register(ata_control.move_ant_group, ants, 'atagr', 'none')
     atexit.register(ata_control.park_antennas, ants)
+    print(f"{datetime.now()} -- end {', '.join(ants)}", file=fplog)
 elif args.cmd == 'freq':
     ata_control.set_freq(args.freq, [ants[0]], lo='d')
     ata_control.autotune([ants[0]])
@@ -29,5 +36,11 @@ elif args.cmd == 'freq':
     ata_control.set_atten_thread([[f'{ant}x', f'{ant}y'] for ant in ants], [[att, att] for ant in ants])
     with open('metadata.yaml', 'w') as fp:
         print(f"fcen: {args.freq}", file=fp)
+    print(f"{datetime.now()} -- freq {args.freq}", file=fplog)
 elif args.cmd == 'move':
     ata_control.set_az_el([ants[0]], args.az, args.el)
+    print(f"{datetime.now()} -- az {args.az}, el {args.el}", file=fplog)
+elif args.cmd == 'note':
+    print(f"{datetime.now()} -- Note: {args.note}", file=fplog)
+
+fplog.close()
