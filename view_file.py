8fa2b9eea339f5087b1637fd753fab71706bc999
DAVID DEBOER <ddeboer@berkeley.edu>
2023-12-22 11:59:20 -0800

    add options and cleanup view_file

--- view_file.py
+++ view_file.py
@@ -2,25 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 from astropy.time import Time
+import numpy as np
 
 
 class Data:
-    def __init__(self, fn, datakey='data'):
+    def __init__(self, fn):
+        self.filename = fn
         with h5py.File(fn, 'r') as fp:
-            self.data = np.array(fp[datakey])
+            self.data = np.array(fp['data'])
             self.jdstart = np.float64(fp['tstart'])  # jd
             self.jdstop = np.float64(fp['tstop'])  # jd
             self.fcen = np.float64(fp['fcen'])  # MHz
             self.bw = np.float64(fp['bw'])  # MHz
         self.tstart = Time(self.jdstart, format='jd')
-        self.tsop = Time(self.jdstop, format='jd')
+        self.tstop = Time(self.jdstop, format='jd')
         self.fmin = self.fcen - self.bw / 2.0
         self.fmax = self.fcen + self.bw / 2.0
 
-    def wf(self, num_xticks=10, num_yticks=4, colorbar=False):
-        plt.imshow(np.log10(self.data))
-        if colorbar:
-            plt.colorbar()
+    def header(self):
+        """Print information about the data."""
+
+        print(f"Filename: {self.filename}")
+        print(f"Data shape: {np.shape(self.data)}")
+        print(f"Start: {self.tstart.datetime} (jd={self.jdstart})")
+        print(f"Stop: {self.tstop.datetime} (jd={self.jdstop})")
+        print(f"Freq:  {self.fmin:.2f} - {self.fmax:.2f} MHz  (cf = {self.fcen}, bw = {self.bw} MHz)")
+
+    def wf(self, **kwargs):
+        """
+        Make a waterfall plot of the data.
+
+        Parameters:
+        -----------
+        kwargs:
+            num_xticks (int), num_yticks (int), colorbar (bool), log (bool)
+
+        """
+        if 'log' in kwargs and not kwargs['log']:
+            plt.imshow(self.data)
+        else:  # Default to using log
+            plt.imshow(np.log10(self.data))
+        if 'colorbar' in kwargs and not kwargs['colorbar']:
+            pass
+        else:
+            plt.colorbar()  # Default to showing colorbar
+        if 'xticks' in kwargs:
+            num_xticks = int(kwargs['xticks'])
+        else:  # Default to 10
+            num_xticks = 10
+        if 'yticks' in kwargs:
+            num_yticks = int(kwargs['yticks'])
+        else:  # Default to 4
+            num_yticks = 4
+
         plt.xticks(np.linspace(0, len(self.data[0]), num_xticks), [f"{x:.2f}" for x in np.linspace(self.fmin, self.fmax, num_xticks)])
         jds = np.linspace(self.jdstart, self.jdstop, num_yticks)
         apt = Time(jds, format='jd')
@@ -29,35 +63,28 @@ class Data:
         plt.xlabel('MHz')
         plt.title(self.tstart.datetime.strftime('%Y-%m-%d'))
 
-    def plot(self):
+    def plot(self, **kwargs):
+        """Make a 2-D plot of the spectra."""
+
+        if 'log' not in kwargs:
+            kwargs['log'] = False
+
         for data in self.data:
-            plt.plot(data)
+            if kwargs['log']:
+                plt.plot(np.log10(data))
+            else:
+                plt.plot(data)
 
 
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('fn', help="Name of hdf5 datafile")
+    ap.add_argument('-x', '--xticks', help="Number of xticks in waterfall [10]", format=int, default=10)
+    ap.add_argument('-y', '--yticks', help="Number of yticks to use in waterfall [4]", format=int, default=4)
+    ap.add_argument('-c', '--colorbar', help="Flag to use colorbar [True]", format=bool, default=True)
+    ap.add_argument('-l', '--log', help="Flag to take log10 of data [True]", format=bool, default=True)
     args = ap.parse_args()
     obs = Data(args.fn)
-    obs.wf()
+    obs.wf(**vars(args))
     plt.show()
-
-### IGNORE BELOW
-# ATA observing...
-def vfDEPR(fn, key='data'):
-    if fn.endswith('h5') or fn.endswith('hdf5'):
-        ftype = 'real'
-    else:
-        ftype = 'complex'
-    if ftype == 'complex':
-        data = np.fromfile(fn, dtype=np.complex64, count=int(1024*1024))
-        plt.specgram(data, NFFT=8192, Fs=1)
-        plt.show()
-    else:
-        with h5py.File(fn, 'r') as fp:
-            data = np.array(fp[key])
-            for i in range(len(data)):
-                plt.plot(data[i])
-        plt.show()
-    return data
