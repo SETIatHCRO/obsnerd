c39a6ab5a595e429490f4382e67ca225530a0091
DAVID DEBOER <ddeboer@berkeley.edu>
2023-12-23 15:56:39 -0800

    changed obsnerd script to use payload

--- obsnerd.py
+++ obsnerd.py
@@ -7,13 +7,11 @@ import metadata
 
 ap = argparse.ArgumentParser()
 ap.add_argument('cmd', help="Action [start, move, freq, end, note]", choices=['start', 'freq', 'move', 'end', 'note'])
-ap.add_argument('--az', help="Azimuth in deg", type=float, default=121.958)
-ap.add_argument('--el', help="Elevation in deg", type=float, default=23.603)
-ap.add_argument('-f', '--freq', help="Frequency in MHz", type=float, default=1680.0)
-ap.add_argument('-n', '--note', help="Note to add", default=None)
+ap.add_argument('payload', help="Argument for command.", nargs='?', default=None)
 args = ap.parse_args()
 
-ants = ['1a', '1f', '5c']  # List of antennas that we want to use (the USRPs can use 2 antennas at a time only)
+ants=['1a', '1f', '5c']
+defaults = argparse.Namespace(az=121.958, el=23.603, freq=1680.0)
 
 # GOES-16
 # cfreq = 1680
@@ -22,20 +20,32 @@ ants = ['1a', '1f', '5c']  # List of antennas that we want to use (the USRPs can
 
 if args.cmd == 'start':
     ata_control.move_ant_group(ants, 'none', 'atagr')
-    metadata.onlog(f"start {', '.join(ants)}")
+    metadata.onlog(f"start: {', '.join(ants)}")
 elif args.cmd == 'end':
     atexit.register(ata_control.move_ant_group, ants, 'atagr', 'none')
     atexit.register(ata_control.park_antennas, ants)
-    metadata.onlog(f"end {', '.join(ants)}")
+    metadata.onlog(f"end: {', '.join(ants)}")
 elif args.cmd == 'freq':
-    metadata.onlog(f"fcen: {args.freq}")
-    ata_control.set_freq(args.freq, [ants[0]], lo='d')
+    freq = defaults.freq if args.payload is None else float(args.payload)
+    metadata.onlog(f"fcen: {freq}")
+    ata_control.set_freq(freq, [ants[0]], lo='d')
     ata_control.autotune([ants[0]])
     att = 20  # Attenuation in dB
     ata_control.rf_switch_thread(ants)
     ata_control.set_atten_thread([[f'{ant}x', f'{ant}y'] for ant in ants], [[att, att] for ant in ants])
 elif args.cmd == 'move':
-    ata_control.set_az_el([ants[0]], args.az, args.el)
-    metadata.onlog(f"az: {args.az}, el: {args.el}")
+    if args.payload is None:
+        az, el = defaults.az, defaults.el
+    elif ',' in args.payload:
+        az, el = [float(x) for x in args.payload.split(',')]
+    else:
+        print(f"Invalid move argument - need az,el")
+        az = None
+    if az is not None:
+        ata_control.set_az_el([ants[0]], az, el)
+        metadata.onlog(f"az: {az}, el: {el}")
 elif args.cmd == 'note':
-    metadata.onlog(f"note: {args.note}")
+    if args.payload is None:
+        print("Need to include a note.")
+    else:
+        metadata.onlog(f"note: {args.payload}")
