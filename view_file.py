18f2c9287c4c88d26ed0578e8f0af2bb3db68037
DAVID DEBOER <ddeboer@berkeley.edu>
2023-12-27 19:43:53 -0800

    finished timezone handling etc

--- view_file.py
+++ view_file.py
@@ -8,9 +8,25 @@ from datetime import timedelta
 from copy import copy
 
 
+def proc_plot_kwargs(kwargs, defaults):
+    for key, val in defaults.items():
+        if key not in kwargs:
+            kwargs[key] = val
+    kwargs['mult'] = 1.0
+    kwargs['ylabel'] = 'linear'
+    if kwargs['log']:
+        kwargs['ylabel'] = 'log'
+    if  kwargs['dB']:
+        kwargs['mult'] = 10.0
+        kwargs['log'] = True
+        kwargs['ylabel'] = 'dB'
+    return kwargs
+
+
 class Data:
-    def __init__(self, fn):
+    def __init__(self, fn, timezone=-8.0):
         self.filename = fn
+        self.timezone = timezone
         self.name, self.datetime = self._parse_fn()
         with h5py.File(fn, 'r') as fp:
             self.data = np.array(fp['data'])
@@ -41,8 +57,10 @@ class Data:
 
         print(f"Filename: {self.filename}")
         print(f"Data shape: {np.shape(self.data)}")
-        print(f"Start: {self.tstart.datetime} (jd={self.jdstart})")
-        print(f"Stop: {self.tstop.datetime} (jd={self.jdstop})")
+        print(f"Start: UTC {self.tstart.datetime} (jd={self.jdstart})")
+        print(f"\tLocal: {self.tstart.datetime + timedelta(hours=self.timezone)}")
+        print(f"Stop: UTC {self.tstop.datetime} (jd={self.jdstop})")
+        print(f"\tLocal: {self.tstop.datetime + timedelta(hours=self.timezone)}")
         print(f"Freq:  {self.fmin:.2f} - {self.fmax:.2f} MHz  (cf = {self.fcen}, bw = {self.bw} MHz)")
 
     def wf(self, **kwargs):
@@ -52,57 +70,61 @@ class Data:
         Parameters:
         -----------
         kwargs:
-            num_xticks (int), num_yticks (int), colorbar (bool), log (bool)
+            num_xticks (int), num_yticks (int), colorbar (bool), log (bool), dB
 
         """
-        if 'log' in kwargs and not kwargs['log']:
-            plt.imshow(self.data)
-        else:  # Default to using log
-            plt.imshow(np.log10(self.data))
-        if 'colorbar' in kwargs and not kwargs['colorbar']:
-            pass
+        defaults = {'log': True, 'dB': False, 'colorbar': True, 'xticks': 10, 'yticks': 4}
+        kwargs = proc_plot_kwargs(kwargs, defaults)
+
+        num_xticks = kwargs['xticks']
+        num_yticks = kwargs['yticks']
+
+        if kwargs['log']:
+            plt.imshow(kwargs['mult'] * np.log10(self.data))
         else:
-            plt.colorbar()  # Default to showing colorbar
-        if 'xticks' in kwargs:
-            num_xticks = int(kwargs['xticks'])
-        else:  # Default to 10
-            num_xticks = 10
-        if 'yticks' in kwargs:
-            num_yticks = int(kwargs['yticks'])
-        else:  # Default to 4
-            num_yticks = 4
+            plt.imshow(self.data)
+        if kwargs['colorbar']:
+            plt.colorbar()
 
         plt.xticks(np.linspace(0, len(self.data[0]), num_xticks), [f"{x:.2f}" for x in np.linspace(self.fmin, self.fmax, num_xticks)])
         jds = np.linspace(self.jdstart, self.jdstop, num_yticks)
         apt = Time(jds, format='jd')
-        yticks = [x.strftime("%H:%M:%S") for x in apt.datetime]
+        yticks = [(x + timedelta(hours=self.timezone)).strftime("%H:%M:%S") for x in apt.datetime]
         plt.yticks(np.linspace(0, len(self.data), num_yticks), yticks)
         plt.xlabel('MHz')
+        plt.ylabel(f"UTC{'+' if self.timezone>0 else '-'}{abs(self.timezone):.0f}")
         plt.title(self.tstart.datetime.strftime('%Y-%m-%d'))
 
     def spectra(self, **kwargs):
         """Make a 2-D plot of the spectra."""
-        if 'log' not in kwargs:
-            kwargs['log'] = False
+        defaults = {'log': False, 'dB': False}
+        kwargs = proc_plot_kwargs(kwargs, defaults)
+
+        df = (self.fmax - self.fmin) / len(self.data[0])
+        f = []
+        this_f = self.fmin + 0.0
+        for i in range(len(self.data[0])):
+            f.append(this_f)
+            this_f += df
 
         for data in self.data:
             if kwargs['log']:
-                plt.plot(np.log10(data))
+                plt.plot(f, kwargs['mult'] * np.log10(data))
             else:
-                plt.plot(data)
+                plt.plot(f, data)
         plt.grid()
         plt.xlabel('MHz')
+        plt.ylabel(kwargs['ylabel'])
 
     def series(self, **kwargs):
-        if 'log' not in kwargs:
-            kwargs['log'] = False
-        if 'tz' not in kwargs:
-            kwargs['tz'] = 8.0
+        defaults = {'log': False, 'dB': False}
+        kwargs = proc_plot_kwargs(kwargs, defaults)
+
         self.plot_max = 0.0
         self.plot_min = 1E20
         dt = (self.tstop.datetime - self.tstart.datetime) / len(self.data[:,0])
         t = []
-        this_t = self.tstart.datetime
+        this_t = self.tstart.datetime + timedelta(hours=self.timezone)
         for i in range(len(self.data[:, 0])):
             t.append(this_t)
             this_t += dt
@@ -122,11 +144,11 @@ class Data:
         if self.datetime is not None:
             self.expected(self.datetime, kwargs['tz'])
         plt.grid()
-        plt.xlabel('UTC')
-        
+        plt.xlabel(f"UTC{'+' if self.timezone>0 else '-'}{abs(self.timezone):.0f}")
+        plt.ylabel(kwargs['ylabel'])
 
     def expected(self, ts, tz_offset=0.0):
-        ts = Time(ts).datetime + timedelta(hours=tz_offset)
+        ts = Time(ts).datetime
         plt.plot([ts, ts], [self.plot_min, self.plot_max], color='k', lw=3)
 
 if __name__ == '__main__':
@@ -138,8 +160,9 @@ if __name__ == '__main__':
     ap.add_argument('-y', '--yticks', help="Number of yticks to use in waterfall [4]", type=int, default=4)
     ap.add_argument('-c', '--colorbar', help="Flag to hide colorbar", action='store_false')
     ap.add_argument('-l', '--log', help="Flag to take log10 of data", action='store_true')
-    ap.add_argument('--tz', help="Timezone offset in hours [8]", type=float, default=8.0)
+    ap.add_argument('-d', '--dB', help="Flag to convert to dB", action='store_true')
+    ap.add_argument('--tz', help="Timezone offset to UTC in hours [-8.0]", type=float, default=-8.0)
     args = ap.parse_args()
-    obs = Data(args.fn)
+    obs = Data(args.fn, args.tz)
     getattr(obs, args.plot_type)(**vars(args))
     plt.show()
