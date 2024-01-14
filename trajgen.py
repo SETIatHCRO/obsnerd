6f7b23bd7967aaac76ee90ff27f54fbcd99f2500
DAVID DEBOER <ddeboer@berkeley.edu>
2024-01-13 16:15:33 -0800

    working trajectory class

--- trajgen.py
+++ trajgen.py
@@ -11,6 +11,9 @@ EPHEM_FILENAME = 'track.ephem'
 TRAJECTORY_FILENAME = 'track'
 TRACK_LOG_FILENAME = 'track.log'
 hcro = EarthLocation(lat=40.8178049*u.deg, lon=-121.4695413*u.deg, height=986*u.m)
+print("GET LIMIT VALUES")
+EL_TRACK_LIMIT = 0.9
+AZ_TRACK_LIMIT = 1.1
 
 class Track:
     """
@@ -35,9 +38,18 @@ class Track:
         s = ''
         for i, lbl in zip([0, -1], ['Start at:', 'End at:']):
             s += f"{lbl} {self.obstime[i]} UTC\n"
-            s += f"\tl={self.l[i]:.3f}, b={self.b[i]:.3f}\n"
-            s += f"\tRA={self.ra[i]:.3f}, Dec={self.dec[i]:.3f}\n"
-            s += f"\tAz={self.az[i]:.3f}, El={self.el[i]:.3f}\n"
+            try:
+                s += f"\tl={self.l[i]:.3f}, b={self.b[i]:.3f}\n"
+            except IndexError:
+                pass
+            try:
+                s += f"\tRA={self.ra[i]:.3f}, Dec={self.dec[i]:.3f}\n"
+            except IndexError:
+                pass
+            try:
+                s += f"\tAz={self.az[i]:.3f}, El={self.el[i]:.3f}\n"
+            except IndexError:
+                pass
         return s
 
     def add(self, **kwargs):
@@ -56,7 +68,8 @@ class Track:
         tai = np.array(self.timestamp, dtype=int)
         az = np.array(self.az, dtype=float)
         el = np.array(self.el, dtype=float)
-        if not len(self.ir):
+        if True:
+            print("NEED TO GET DISTANCES FROM SOPP")
             self.ir = np.zeros(len(tai)) + 1E-20
         ephem = ((np.array([tai, az, el, self.ir], dtype=object)))
         self.ephemtxt = np.savetxt(filename, ephem.T, fmt='%i  %.5f  %.5f  %.10E')
@@ -67,157 +80,159 @@ class Track:
                             az=self.az, el=self.el, ra=self.ra, dec=self.dec, 
                             l=self.l, b=self.b, ir=self.ir, allow_pickle=True)
 
-def _get_track_times(start, duration, step):
-    """
-    Parameters
-    ----------
-    start : Time
-    duration : float
-        duration in seconds
-    step : float
-        step in seconds
-
-    """
-    track_Times = start + np.arange(0.0, duration, step) * u.second
-    track_tsns = int(start.unix_tai * 1E9) + np.arange(0, int(duration * 1E9), int(step * 1E9))
-    return track_Times, track_tsns
-
-def galactic(ttype='gal', start_time='2023-12-31 23:59:59', using=0.0, el_starting=30.0, time_to_track=20.0, anglerate=0.1, tstep=1.0, tz=-8.0):
-    """
-    Parameters
-    ----------
-    start_time : str etc
-        isoformat or anything Time handles in local time used to start checking
-    using : float
-        galactic b value to use [deg]
-    el_starting : float
-        Elevation to start using track [deg]
-    time_to_track : float
-        Length of track in minutes
-    anglerate : float
-        galactic l step for track [deg/sec]
-    tstep : float
-        Trajectory time step in seconds
-    tz : float
-        Hours from UTC
-
-    """
-    if ttype != 'gal':
-        print(f"Incorrect ttype: {ttype}")
-        return
-    tz = tz * u.hour
-    start_time = Time(start_time) - tz
-    fixed_b = using * u.deg
-
-    # Get starting galactic longitude to nearest 1/2 degree and plot
-    gp_l = np.arange(0.0, 359.9, 0.1) * u.deg
-    gp_b = np.ones(len(gp_l)) * fixed_b
-    gal = SkyCoord(frame='galactic', l=gp_l, b=gp_b)
-    radec = gal.transform_to('icrs')
-    azel = radec.transform_to(AltAz(obstime=start_time, location=hcro))
-
-    above_horizon = np.where(azel.alt.value > el_starting)
-    if not len(above_horizon[0]):
-        print("Nothing above the supplied horizon")
-        return False
-    start_horizon = above_horizon[0][0]
-    starting_l = gal.l.value[start_horizon] * u.deg
-    plt.figure('At T=0')
-    plt.subplot(211)
-    plt.plot(radec.ra.value[above_horizon], radec.dec.value[above_horizon], '.', label='radec-init')
-    plt.plot(azel.az.value[above_horizon], azel.alt.value[above_horizon], '.', label='azel-init')
-
-    # Get the track
-    track_times, track_tsns = _get_track_times(start_time, time_to_track * 60.0, float(tstep))
-    this_track = Track(f'Galactic b={using}', timestamp=track_tsns, obstime=track_times)
-    tstep = tstep * u.second
-    anglerate = anglerate * u.deg / u.second
-
-    for i in range(len(track_times)):
-        this_l = starting_l  + i*anglerate*tstep
-        if this_l > 360.0*u.deg:
-            this_l = this_l - 360.0*u.deg
-        gal = SkyCoord(frame='galactic', l=this_l, b=fixed_b)
-        radec = gal.transform_to('icrs')
-        azel = radec.transform_to(AltAz(obstime=track_times[i], location=hcro))
-        this_track.add(az=azel.az.value, el=azel.alt.value, ra=radec.ra.value, dec=radec.dec.value, l=this_l.value, b=fixed_b.value)
-
-    this_track.log_track(TRACK_LOG_FILENAME)
-    this_track.ephem_file(EPHEM_FILENAME)
-    this_track.full_file(TRAJECTORY_FILENAME)
-
-    plt.plot(this_track.az, this_track.el, '.', lw=4, label='azel-track')
-    plt.grid()
-    plt.legend()
-    plt.xlabel('RA/Az')
-    plt.ylabel('Dec/El')
-
-    return this_track
+    def rates(self):
+        dt = np.diff(self.timestamp) / 1E9
+        self.dazdt = np.diff(self.az) / dt
+        self.deldt = np.diff(self.el) / dt
 
 
-def from_file(ttype='file', start_time='2023-12-31 23:59:59', using='', el_starting=30.0, time_to_track=20.0, anglerate=0.1, tstep=1.0, tz=-8.0):
-    """
-    Parameters
-    ----------
-    start_time : str etc
-        isoformat or anything Time handles in local time used to start checking
-    using : float
-        galactic b value to use [deg]
-    el_starting : float
-        Elevation to start using track [deg]
-    time_to_track : float
-        Length of track in minutes
-    anglerate : float
-        galactic l step for track [deg/sec]
-    tstep : float
-        Trajectory time step in seconds
-    tz : float
-        Hours from UTC
+class Trajectory:
+    def __init__(self, start_time='2023-12-31 23:59:59', time_to_track=20.0, tstep=1.0, el_horizon=30.0, tz=-8.0):
+        """
+        Parameters
+        ----------
+        start_time : str etc
+            isoformat or anything Time handles in local time used to start checking
+        time_to_track : float
+            Length of track in minutes
+        tstep : float
+            Trajectory time step in seconds
+        el_horizon : float
+            Elevation to start using track [deg]
+        tz : float
+            Hours from UTC
 
-    """
-    from scipy import interpolate
-    if ttype != 'file':
-        print(f"Incorrect ttype: {ttype}")
-        return
-    # Set up track
-    tz = tz * u.hour
-    start_time = Time(start_time) - tz
-    track_times, track_tsns = _get_track_times(start_time, time_to_track * 60.0, float(tstep))
-    this_track = Track('Out', timestamp=track_tsns, obstime=track_times)
+        """
+        self.valid = True
+        self.tz = tz * u.hour
+        self.start_time = Time(start_time) - self.tz
+        self.el_horizon = float(el_horizon)
+        self.time_to_track = float(time_to_track) * 60.0 * u.second
+        self.tstep = float(tstep) * u.second
+        self.track_Time = self.start_time + np.arange(0.0, self.time_to_track.value, self.tstep.value) * u.second
+        self.track_tsns = int(self.start_time.unix_tai * 1E9) + np.arange(0, int(self.time_to_track.value * 1E9), int(self.tstep.value * 1E9))
 
-    tstep = tstep * u.second
-    anglerate = anglerate * u.deg / u.second
+    def galactic(self, b=0.0, rate=0.1):
+        self.trajectory_type = 'galactic'
+        self.b = b * u.deg
+        self.rate = rate * u.deg / u.second
 
-    # Read input file
-    overlap = False
-    input_track = Track('In')
-    with open(using, 'r') as fp:
-        for line in fp:
-            data = line.split(',')
-            obstime = Time(data[0], format='isot')
-            if not overlap and obstime >= track_times[0] and obstime <= track_times[-1]:
-                overlap = True
-            ts = int(obstime.unix_tai * 1E9)
-            input_track.add(timestamp=ts, obstime=obstime, az=float(data[1]), el=float(data[2]), ir=1.0 / float(data[3]))
-    if not overlap:
-        print("This supplied file and desired track don't overlap")
-        return False
-    input_track.obstime = Time(input_track.obstime)
+        # Get starting galactic longitude to nearest 1/10 degree and plot
+        self.T0 = argparse.Namespace()
+        gp_l = np.arange(0.0, 359.9, 0.1) * u.deg
+        gp_b = np.ones(len(gp_l)) * self.b
+        self.T0.gal = SkyCoord(frame='galactic', l=gp_l, b=gp_b)
+        self.T0.radec = self.T0.gal.transform_to('icrs')
+        self.T0.azel = self.T0.radec.transform_to(AltAz(obstime=self.start_time, location=hcro))
 
-    # Overlap, so fit az, el, ir
-    f = interpolate.interp1d(input_track.timestamp, input_track.az, fill_value=0.0, bounds_error=False)
-    this_track.az = f(this_track.timestamp)
-    f = interpolate.interp1d(input_track.timestamp, input_track.el, fill_value=0.0, bounds_error=False)
-    this_track.el = f(this_track.timestamp)
-    f = interpolate.interp1d(input_track.timestamp, input_track.ir, fill_value=0.0, bounds_error=False)
-    this_track.ir = f(this_track.timestamp)
+        self.T0.above_horizon = np.where(self.T0.azel.alt.value > self.el_horizon)
+        if not len(self.T0.above_horizon[0]):
+            print("Nothing above the supplied horizon")
+            self.valid = False
+            return
+        starting_l = self.T0.gal.l[self.T0.above_horizon[0][0]]
 
-    plt.figure('Input')
-    plt.subplot(211)
-    plt.plot(input_track.obstime.datetime, input_track.az)
-    plt.plot(input_track.obstime.datetime, input_track.el)
+        # Get the track
+        self.track = Track(f'Galactic b={self.b}', timestamp=self.track_tsns, obstime=self.track_Time)        
+        R = self.rate*self.tstep
+        for i in range(len(self.track_Time)):
+            this_l = starting_l  + i*R
+            if this_l > 360.0*u.deg:
+                this_l = this_l - 360.0*u.deg
+            gal = SkyCoord(frame='galactic', l=this_l, b=self.b)
+            radec = gal.transform_to('icrs')
+            azel = radec.transform_to(AltAz(obstime=self.track_Time[i], location=hcro))
+            self.track.add(az=azel.az.value, el=azel.alt.value, ra=radec.ra.value, dec=radec.dec.value, l=this_l.value, b=self.b.value)
+        self.track.rates()
 
-    return this_track
+    def from_file(self, filename):
+        """
+        Parameters
+        ----------
+        filename : str
+            File containing the data
+
+        """
+        self.trajectory_type = 'from_file'
+        from scipy import interpolate
+        self.track = Track('Out', timestamp=self.track_tsns, obstime=self.track_Time)
+        # Read input file
+        overlap = False
+        self.input_track = Track('In')
+        with open(filename, 'r') as fp:
+            for line in fp:
+                data = line.split(',')
+                obstime = Time(data[0], format='isot')
+                if not overlap and obstime >= self.track_Time[0] and obstime <= self.track_Time[-1]:
+                    overlap = True
+                ts = int(obstime.unix_tai * 1E9)
+                self.input_track.add(timestamp=ts, obstime=obstime, az=float(data[1]), el=float(data[2]), ir=1.0 / float(data[3]))
+        if not overlap:
+            print("This supplied file and desired track don't overlap")
+            self.valid = False
+        self.input_track.obstime = Time(self.input_track.obstime)
+
+        # Overlap, so fit az, el, ir
+        f = interpolate.interp1d(self.input_track.timestamp, self.input_track.az, fill_value=0.0, bounds_error=False)
+        self.track.az = f(self.track.timestamp)
+        f = interpolate.interp1d(self.input_track.timestamp, self.input_track.el, fill_value=0.0, bounds_error=False)
+        self.track.el = f(self.track.timestamp)
+        f = interpolate.interp1d(self.input_track.timestamp, self.input_track.ir, fill_value=0.0, bounds_error=False)
+        self.track.ir = f(self.track.timestamp)
+        self.track.rates()
+
+    def write(self):
+        self.track.log_track(TRACK_LOG_FILENAME)
+        self.track.ephem_file(EPHEM_FILENAME)
+        self.track.full_file(TRAJECTORY_FILENAME)
+
+    def plot(self):
+        if self.trajectory_type == 'galactic':
+            self._plot_galactic()
+        elif self.trajectory_type == 'from_file':
+            self._plot_from_file()
+        plt.figure('Rates')
+        t_extremes = [self.track.obstime.datetime[1], self.track.obstime.datetime[-1]]
+        sgn = '--' if np.any(self.track.dazdt < 0.0) else '-'
+        plt.plot(self.track.obstime.datetime[1:], abs(self.track.dazdt), 'k'+sgn, label='daz/dt')
+        plt.plot(t_extremes, [AZ_TRACK_LIMIT, AZ_TRACK_LIMIT], 'k-.', lw=3)
+        sgn = '--' if np.any(self.track.deldt < 0.0) else '-'
+        plt.plot(self.track.obstime.datetime[1:], abs(self.track.deldt), 'b'+sgn, label='del/dt')
+        plt.plot(t_extremes, [EL_TRACK_LIMIT, EL_TRACK_LIMIT], 'b-.', lw=3)
+        plt.legend()
+        plt.ylabel('[deg/sec]')
+        plt.grid()
+
+    def _plot_galactic(self):
+        plt.figure('Galactic')
+        plt.subplot(211)
+        plt.plot(self.T0.radec.ra.value[self.T0.above_horizon], self.T0.radec.dec.value[self.T0.above_horizon], '.', label='radec-init')
+        plt.plot(self.T0.azel.az.value[self.T0.above_horizon], self.T0.azel.alt.value[self.T0.above_horizon], '.', label='azel-init')
+        plt.plot(self.track.az, self.track.el, '.', lw=4, label='azel-track')
+        plt.grid()
+        plt.legend()
+        plt.xlabel('RA/Az [deg]')
+        plt.ylabel('Dec/El [deg]')
+        plt.subplot(212)
+        plt.plot(self.track.obstime.datetime, self.track.az, '.', label='Az')
+        plt.plot(self.track.obstime.datetime, self.track.el, '.', label='El')
+        plt.plot(self.track.obstime.datetime, self.track.ra, '.', label='RA')
+        plt.plot(self.track.obstime.datetime, self.track.dec, '.', label='Dec')
+        plt.xlabel('Time')
+        plt.ylabel('[deg]')
+        plt.grid()
+        plt.legend()
+
+    def _plot_from_file(self):
+        plt.figure('From_file')
+        plt.plot(self.input_track.obstime.datetime, self.input_track.az, 'k', label='Az')
+        plt.plot(self.input_track.obstime.datetime, self.input_track.el, 'b', label='El')
+        plt.plot(self.track.obstime.datetime, self.track.az, 'ko')
+        plt.plot(self.track.obstime.datetime, self.track.el, 'bo')
+        plt.xlabel('Time')
+        plt.ylabel('[deg]')
+        plt.legend()
+        plt.grid()
 
 if __name__ == '__main__':
     import argparse
@@ -225,9 +240,10 @@ if __name__ == '__main__':
     ap.add_argument('ttype', help="Type of trajectory ([gal]/file)", nargs='?', default='gal')
     ap.add_argument('-s', '--start_time', help="Time to start checking ['now+5' min]", default='now+5')
     ap.add_argument('-u', '--using', help="Galactic latitude to use if 'gal' or filename to use [0.0 deg]", default=0.0)
-    ap.add_argument('-e', '--el_starting', help='Elevation to start at [30.0 deg]', type=float, default=30.0)
+    ap.add_argument('-e', '--el_horizon', help='Elevation to start at [30.0 deg]', type=float, default=30.0)
     ap.add_argument('-t', '--time_to_track', help='Length of track [10.0 min]', type=float, default=10.0)
-    ap.add_argument('-a', '--anglerate', help="Galactic longitude step in trajectory [0.1 deg/sec]", type=float, default=0.1)
+    ap.add_argument('-r', '--rate', help="Galactic longitude step in trajectory [0.1 deg/sec]", type=float, default=0.1)
+    ap.add_argument('-w', '--write_out', help="Write out the files.", action='store_true')
     ap.add_argument('--tstep', help="Time step in trajectory [1 sec]", type=float, default=1.0)
     ap.add_argument('--tz', help="Hours from UTC [0.0 hours]", type=float, default=0.0)
     args = ap.parse_args()
@@ -240,18 +256,15 @@ if __name__ == '__main__':
         elif len(offsm) == 2:
             args.start_time = datetime.datetime.now() - datetime.timedelta(minutes=float(offsm[1]))
 
+    traj = Trajectory(start_time=args.start_time, time_to_track=args.time_to_track, tstep=args.tstep, el_horizon=args.el_horizon, tz=args.tz)
+
     if args.ttype == 'gal':
-        traj = galactic(**vars(args))
+        traj.galactic(b=float(args.using), rate=args.rate)
     else:
-        traj = from_file(**vars(args))
-    if traj:
-        print(traj.name)
-        plt.subplot(212)
-        plt.plot(traj.obstime.datetime, traj.az, '.', label='Az')
-        plt.plot(traj.obstime.datetime, traj.el, '.', label='El')
-        if len(traj.ra):
-            plt.plot(traj.obstime.datetime, traj.ra, '.', label='RA')
-            plt.plot(traj.obstime.datetime, traj.dec, '.', label='Dec')
-        plt.grid()
-        plt.legend()
+        traj.from_file(filename=args.using)
+    if args.write_out:
+        traj.write()
+    if traj.valid:
+        print(traj.track.name)
+        traj.plot()
         plt.show()
