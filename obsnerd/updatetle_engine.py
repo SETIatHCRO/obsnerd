ed968581f6bf8f31d915a2efbcd5b75a001f4583
DAVID DEBOER <ddeboer@berkeley.edu>
2024-01-13 17:11:09 -0800

    moved updatetle to pkg

--- obsnerd/updatetle_engine.py
+++ obsnerd/updatetle_engine.py
@@ -1,8 +1,7 @@
-#! /usr/bin/env python
 import requests
 from html.parser import HTMLParser
 from os import path
-import metadata
+from obsnerd import metadata
 
 
 class DataParser(HTMLParser):
@@ -55,15 +54,3 @@ def updatetle(base_path, base_url):
 
 def update_log():
     metadata.onlog('Updating TLEs')
-
-if __name__ == '__main__':
-    import argparse
-    ap = argparse.ArgumentParser()
-    ap.add_argument('--base-url', dest='base_url', help="Base url for tles",
-                    default='http://celestrak.org/NORAD/elements/')
-    ap.add_argument('--base-path', dest='base_path', help="Base path for tles",
-                    default='./tle')
-    args = ap.parse_args()
-    updatetle(base_path=args.base_path, base_url=args.base_url)
-    update_log()
-
