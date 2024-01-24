faf70cd857ebfa7fcda23d9b5f493d367b783f06
DAVID DEBOER <ddeboer@berkeley.edu>
2024-01-24 12:19:06 -0800

    working version of log class implementation

--- obsnerds/metadata.py
+++ obsnerds/metadata.py
@@ -9,8 +9,8 @@ UTC = timezone(timedelta(0), 'UTC')
 PST = timezone(timedelta(hours=-8), 'PST')
 PDT = timezone(timedelta(hours=-7), 'PDT')
 LOGFILE_DELIMITER = '--'
-LOG_PARAMS_TO_GET = ['tstart', 'source:', 'expected:', 'azel:', 'fcen:', 'bw:', 'session start:',
-                     'end:', 'traj:', 'track:', 'Writing', 'move to:', 'TLEs', 'tstop']
+LOG_ENTRIES_TO_GET = ['tstart', 'source:', 'expected:', 'azel:', 'fcen:', 'bw:', 'session start:',
+                      'end:', 'traj:', 'track:', 'Writing', 'move to:', 'TLEs', 'tstop']
 
 
 # Log functions
@@ -30,48 +30,60 @@ def onlog(notes):
         for note in notes:
             print(f"{ts} -- {note}", file=fp)
 
+class Onlog:
+    def __init__(self, entries=LOG_ENTRIES_TO_GET, delimiter=LOGFILE_DELIMITER, auto_read=False):
+        self.file = ONLOG_FILENAME
+        self.delimiter = delimiter
+        self.entries = entries
+        self.data = {'other': {}}
+        self.latest = {}
+        self.keys = [p.strip(':') for p in self.entries]
+        for key in self.keys:
+            self.data[key] = {}
+            self.latest[key] = ''
+        if auto_read:
+            self.read()
 
-def read_onlog(params=LOG_PARAMS_TO_GET):
-    logdata = {'other': {}}
-    keys = [p.strip(':') for p in params]
-    for key in keys:
-        logdata[key] = {}
-    with open(ONLOG_FILENAME, 'r') as fp:
-        for line in fp:
-            par_not_found = True
-            data = [x.strip() for x in line.split(LOGFILE_DELIMITER)]
-            for key, param in zip(keys, params):
-                if param in data[1]:
-                    par_not_found = False
-                    if key in ['tstart', 'tstop', 'TLEs']:
-                        logdata[key][data[0]] = data[0]
-                    elif key == 'track':
-                        logdata[key].setdefault(data[0], [])
-                        logdata[key][data[0]].append(LOGFILE_DELIMITER.join(data[1:]))
-                    else:
-                        logdata[key][data[0]] = LOGFILE_DELIMITER.join(data[1:])
-            if par_not_found:
-                logdata['other'][data[0]] = line.strip()
-    return logdata
+    def read(self):
+        self.all_timestamps = set()
+        with open(self.file, 'r') as fp:
+            for line in fp:
+                par_not_found = True
+                linedata = [x.strip() for x in line.split(self.delimiter)]
+                timestamp, payload = linedata[0], self.delimiter.join(linedata[1:])
+                self.all_timestamps.add(timestamp)
+                for key, entry in zip(self.keys, self.entries):
+                    if entry in linedata[1]:
+                        par_not_found = False
+                        if key in ['tstart', 'tstop', 'TLEs']:
+                            self.data[key][timestamp] = timestamp
+                            self.latest[key] = timestamp
+                        elif key == 'track':
+                            self.data[key].setdefault(timestamp, [])
+                            self.data[key][timestamp].append(payload)
+                            self.latest[key] = payload
+                        else:
+                            self.data[key][timestamp] = payload
+                            self.latest[key] = payload
+                if par_not_found:
+                    self.data['other'][timestamp] = payload
+        self.all_timestamps = sorted(list(self.all_timestamps))
 
 
-def get_latest_value(param, parse=False):
+def get_latest_value(key, parse=False):
     """
-    Return the latest entry for given param in line.
+    Return the latest entry for given entry in line.
 
     Parameters
     ----------
-    param : str
-        string to search for
+    key : str
+        key to use
     parse : str or False
         if 'timestamp' uses the timestamp
         if str will split on that string and return last index
     """
-    logdata = read_onlog()
-    ts = sorted(logdata[param])
-    if not len(ts):
-        return None
-    val = logdata[param][ts[-1]]
+    log = Onlog(auto_read=True)
+    val = log.latest[key]
     if parse:
         val = val.split(parse)[-1].strip()
     return val
@@ -79,47 +91,37 @@ def get_latest_value(param, parse=False):
 
 def get_summary():
     from copy import copy
+    log = Onlog(auto_read=True)
     headers=['obs', 'start', 'stop', 'source', 'expected', 'az', 'el', 'fcen', 'bw']
     I = {}
     for i, hdr in enumerate(headers):
         I[hdr] = i
-    logdata = read_onlog()
-    tstart = list(logdata['tstart'].keys())
-    source = list(logdata['source'].keys())
-    azel = list(logdata['azel'].keys())
-    expected = list(logdata['expected'].keys())
-    fcen = list(logdata['fcen'].keys())
-    bw = list(logdata['bw'].keys())
-    obs = list(logdata['session start'].keys())
-    tstop = list(logdata['tstop'].keys())
-    tsall = set(tstart + source + azel + expected + fcen + bw + obs + tstop)
-    tsall = sorted(list(tsall))
     table_data = []
     row = ['' for x in range(len(headers))]
     previous = {'fcen': '', 'obs': '', 'az': '', 'el': ''}
-    for this_ts in tsall:
-        if this_ts in obs:
-            row[I['obs']] = (logdata['session start'][this_ts].split(LOGFILE_DELIMITER)[0][15:]).strip()
+    for this_ts in log.all_timestamps:
+        if this_ts in log.data['session start']:
+            row[I['obs']] = (log.data['session start'][this_ts].split(log.delimiter)[0][15:]).strip()
             previous['obs'] = copy(row[I['obs']])
-        if this_ts in tstart:
-            row[I['start']] = logdata['tstart'][this_ts]
-        if this_ts in source:
-            row[I['source']] = logdata['source'][this_ts].split(':')[-1]
-        if this_ts in expected:
-            row[I['expected']] = logdata['expected'][this_ts][9:].strip()
-        if this_ts in azel:
-            payload = logdata['azel'][this_ts].split(':')[-1].split(',')
+        if this_ts in log.data['tstart']:
+            row[I['start']] = log.data['tstart'][this_ts]
+        if this_ts in log.data['source']:
+            row[I['source']] = log.data['source'][this_ts].split(':')[-1]
+        if this_ts in log.data['expected']:
+            row[I['expected']] = log.data['expected'][this_ts][9:].strip()
+        if this_ts in log.data['azel']:
+            payload = log.data['azel'][this_ts].split(':')[-1].split(',')
             row[I['az']] = payload[0]
             row[I['el']] = payload[1]
             previous['az'] = copy(row[I['az']])
             previous['el'] = copy(row[I['el']])
-        if this_ts in fcen:
-            row[I['fcen']] = logdata['fcen'][this_ts].split(':')[-1]
+        if this_ts in log.data['fcen']:
+            row[I['fcen']] = log.data['fcen'][this_ts].split(':')[-1]
             previous['fcen'] = copy(row[I['fcen']])
-        if this_ts in bw:
-            row[I['bw']] = float(logdata['bw'][this_ts].split(':')[-1]) / 1E6
-        if this_ts in tstop:
-            row[I['stop']] = logdata['tstop'][this_ts]
+        if this_ts in log.data['bw']:
+            row[I['bw']] = float(log.data['bw'][this_ts].split(':')[-1]) / 1E6
+        if this_ts in log.data['tstop']:
+            row[I['stop']] = log.data['tstop'][this_ts]
             for par in ['obs', 'fcen', 'az', 'el']:
                 if not len(row[I[par]]):
                     row[I[par]] = previous[par]
