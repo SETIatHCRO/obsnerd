cf81adef0f61eea4cbdf572618d169dfbc4efdce
DAVID DEBOER <ddeboer@berkeley.edu>
2024-11-26 18:07:45 -0800

    starting process of updating obsnerd observing

--- obsnerds/obsnerd_engine.py
+++ obsnerds/obsnerd_engine.py
@@ -4,18 +4,32 @@ except ImportError:
     ata_control = None
 import atexit
 from . import metadata, onutil
+import subprocess
 
 
 class CommandHandler:
-    def __init__(self, payload=None, arg2=None, coord_type=None, group_ants=['1a'], use_ants=['1a']):
-        self.payload = payload
+    def __init__(self, arg1=None, arg2=None, coord_type=None, group_ants=['1a'], use_ants=['1a']):
+        """
+        arg1 : str or None
+            "arg1" for the empty command call
+        arg2 : str or None
+            "arg2" for the empty command call
+        coord_type : str or None
+            supplied coordinate type (azel, radec, source, traj)
+        group_ants : list
+            list of ants to move into the reserved group
+        use_ants : list
+            list of ants to point with command
+
+        """
+        self.arg1 = arg1
         self.arg2 = arg2
         self.coord_type = coord_type
         self.group_ants = group_ants
         self.use_ants = use_ants
     
     def start(self, initials=None):
-        self.initials = self.payload if initials is None else initials
+        self.initials = self.arg1 if initials is None else initials
         if self.initials is None:
             print("Please include your name or initials.")
         elif ata_control is None:
@@ -33,7 +47,7 @@ class CommandHandler:
             metadata.onlog(f"end: {', '.join(self.use_ants)} / {', '.join(self.group_ants)}")
 
     def freq(self, freq=None, att=20):
-        self.freq = float(self.payload) if freq is None else freq
+        self.freq = float(self.arg1) if freq is None else freq
         self.att = att
         metadata.onlog(f"fcen: {self.freq}")
         if ata_control is not None:
@@ -41,9 +55,18 @@ class CommandHandler:
             ata_control.autotune(self.use_ants)
             ata_control.rf_switch_thread(self.use_ants)
             ata_control.set_atten_thread([[f'{ant}x', f'{ant}y'] for ant in self.use_ants], [[self.att, self.att] for ant in self.use_ants])
+    
+    def backend(self, betype='xgpu', project_id='p054'):
+        metadata.onlog(f"Backend {betype} for project {project_id}")
+        if betype == 'xgpu':
+            subprocess.run("ansible-playbook /home/sonata/src/ansible_playbooks/hashpipe/xgpu_record.yml")
+        else:
+            print(f"Invalid backend: {betype} -- no action")
+            return
+        subprocess.run(f"/home/sonata/src/observing_campaign/backend_setup_scripts/set_keys_uvh5_{project_id}.py")
 
     def move(self, location=None, coord_type=None):
-        self.location = self.payload if location is None else location
+        self.location = self.arg1 if location is None else location
         self.coord_type = self.coord_type if coord_type is None else coord_type
 
         if ',' in self.location:
@@ -76,14 +99,14 @@ class CommandHandler:
                 pass
     
     def note(self, notation=None):
-        self.notation = self.payload if notation is None else notation
+        self.notation = self.arg1 if notation is None else notation
         if self.notation is None:
             print("Need to include a note.")
         else:
             metadata.onlog(f"note: {self.notation}")
 
     def source(self, name=None, datestamp=None):
-        self.name = self.payload if name is None else name
+        self.name = self.arg1 if name is None else name
         self.datestamp = self.arg2 if datestamp is None else datestamp
         self.datestamp = onutil.make_datetime(date=self.datestamp)
         metadata.onlog([f'source: {self.name}', f'expected: {self.datestamp.isoformat()}'])
--- scripts/obsnerd.py
+++ scripts/obsnerd.py
@@ -4,11 +4,11 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('cmd', help="Action [start, freq,  move, end, note, source, summary]", choices=['start', 'freq', 'move', 'end', 'note', 'source', 'summary'])
-ap.add_argument('payload', help="Argument for command.", nargs='?', default=None)
-ap.add_argument('arg2', help="If source, associated datetime.", nargs='?', default=None)
+ap.add_argument('arg1', help="Argument for command.", nargs='?', default=None)
+ap.add_argument('arg2', help="If source, associated datetime (secondary argument).", nargs='?', default=None)
 ap.add_argument('-c', '--coord-type', dest='coord_type', help="For <move> coordinate type: azel/radec/source/traj [azel]",
                 choices=['azel', 'radec', 'source', 'traj'], default='azel')
 args = ap.parse_args()
 
-session = oe.CommandHandler(payload=args.payload, arg2=args.arg2, coord_type=args.coord_type)
+session = oe.CommandHandler(arg1=args.arg1, arg2=args.arg2, coord_type=args.coord_type)
 getattr(session, args.cmd)()
