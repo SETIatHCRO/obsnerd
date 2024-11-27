888afd224a1323c33d99cc1a10218bacfebb725d
DAVID DEBOER <ddeboer@berkeley.edu>
2024-11-26 20:33:09 -0800

    new command line obsnerd mode mostly done

--- obsnerds/obsnerd_engine.py
+++ obsnerds/obsnerd_engine.py
@@ -1,5 +1,5 @@
 try:
-    from ATATools import ata_control # type: ignore
+    from ATATools import ata_control  # type: ignore
 except ImportError:
     ata_control = None
 import atexit
@@ -7,75 +7,154 @@ from . import metadata, onutil
 import subprocess
 
 
+
 class CommandHandler:
-    def __init__(self, arg1=None, arg2=None, coord_type=None, group_ants=['1a'], use_ants=['1a']):
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+        self._setantlists()
+
+    def _setvar(self, initargs, kwargs):
+        for key, val in initargs.items():
+            if key in kwargs:  # Definitely change if kwarg
+                setattr(self, key, kwargs[key])
+            elif not hasattr(self, key):  # Only set to initialize value if not already set
+                setattr(self, key, val)
+        self._setantlists()
+
+    def _setantlists(self, special_lists=[]):
+        for ant_list in ['group_ants', 'use_ants']:
+            if hasattr(self, ant_list):
+                if isinstance(getattr(self, ant_list), str):
+                    if getattr(self, ant_list) in special_lists:
+                        setattr(self, ant_list, 'GET WAY TO GET FROM SPECIAL LISTS')
+                    else:
+                        setattr(self, ant_list, getattr(self, ant_list).split(','))
+        if hasattr(self, 'use_ants') and self.use_ants is None:
+            self.use_ants = self.group_ants
+
+    def start(self, **kwargs):
         """
-        arg1 : str or None
-            "arg1" for the empty command call
-        arg2 : str or None
-            "arg2" for the empty command call
-        coord_type : str or None
-            supplied coordinate type (azel, radec, source, traj)
+        Parameters (kwargs)
+        -------------------
+        initials : str
+            Initials of observer
+        project_id : str
+            Project identifier
         group_ants : list
             list of ants to move into the reserved group
-        use_ants : list
-            list of ants to point with command
 
         """
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.coord_type = coord_type
-        self.group_ants = group_ants
-        self.use_ants = use_ants
-    
-    def start(self, initials=None):
-        self.initials = self.arg1 if initials is None else initials
+        self._setvar({'initials': None, 'project_id': None, 'group_ants': ['1a']}, kwargs)
+
         if self.initials is None:
             print("Please include your name or initials.")
         elif ata_control is None:
-            self.test(f'test start:  {self.initials}')
+            self.test(f'test start:  {self.initials} for project {self.project_id}')
         else:
             ata_control.move_ant_group(self.group_ants, 'none', 'atagr')
-            metadata.onlog(f"session start: {self.initials} -- {', '.join(self.use_ants)} / {', '.join(self.group_ants)}")
+        metadata.onlog(f"session start: {self.initials} -- reserving {', '.join(self.group_ants)}")
+        metadata.onlog(f"project_id: {self.project_id}")
 
-    def end(self):
+    def end(self, **kwargs):
+        """
+        Parameters (kwargs)
+        -------------------
+        group_ants : list
+            Antenna list in the group to unreserve
+        use_ants : list
+            Antenna list to park, default use group_ants
+
+        """
+        self._setvar({'group_ants': ['1a'], 'use_ants': None}, kwargs)
+
         if ata_control is None:
             self.test("test end")
         else:
-            atexit.register(ata_control.move_ant_group, self.group_ants, 'atagr', 'none')
             atexit.register(ata_control.park_antennas, self.use_ants)
-            metadata.onlog(f"end: {', '.join(self.use_ants)} / {', '.join(self.group_ants)}")
+            atexit.register(ata_control.move_ant_group, self.group_ants, 'atagr', 'none')
+        metadata.onlog(f"parking: {', '.join(self.use_ants)}")
+        metadata.onlog(f"end: {', '.join(self.group_ants)}")
 
-    def freq(self, freq=None, att=20):
-        self.freq = float(self.arg1) if freq is None else freq
-        self.att = att
-        metadata.onlog(f"fcen: {self.freq}")
-        if ata_control is not None:
-            ata_control.set_freq(self.freq, self.use_ants, lo='d')
+    def freq(self, **kwargs):
+        """
+        Parameters (kwargs)
+        -------------------
+        frequency : float
+            Frequency of observation [GHz]
+        lo : str
+            LO to use A/B/C/D
+        use_ants : list or None
+            List of antennas to use, if None, assume group_ants
+        attenuation : int
+            Attenuation setting
+
+        """
+        self._setvar({'frequency': None, 'lo': 'A', 'use_ants': None, 'attenuation': 20, 'group_ants': ['1a']}, kwargs)
+        if self.frequency is None:
+            print("Need a frequency in GHz -- no action")
+            return
+
+        metadata.onlog(f"fcen: {self.frequency}")
+        metadata.onlog(f"lo: {self.lo}")
+        metadata.onlog(f"antennas:  {(', ').join(self.use_ants)}")
+        if ata_control is None:
+            self.test('Test freq')
+        else:
+            ata_control.set_freq(self.frequency, self.use_ants, lo=self.lo.lower())
             ata_control.autotune(self.use_ants)
             ata_control.rf_switch_thread(self.use_ants)
-            ata_control.set_atten_thread([[f'{ant}x', f'{ant}y'] for ant in self.use_ants], [[self.att, self.att] for ant in self.use_ants])
+            ata_control.set_atten_thread([[f'{ant}x', f'{ant}y'] for ant in self.use_ants], [[self.attenuation, self.attenuation] for ant in self.use_ants])
     
-    def backend(self, betype='xgpu', project_id='p054'):
-        metadata.onlog(f"Backend {betype} for project {project_id}")
-        if betype == 'xgpu':
-            subprocess.run("ansible-playbook /home/sonata/src/ansible_playbooks/hashpipe/xgpu_record.yml")
-        else:
-            print(f"Invalid backend: {betype} -- no action")
-            return
-        subprocess.run(f"/home/sonata/src/observing_campaign/backend_setup_scripts/set_keys_uvh5_{project_id}.py")
+    def backend(self, **kwargs):
+        """
+        Parameters (kwargs)
+        -------------------
+        backend : str
+            Name of backend to use
+        project_id : str
+            ID of project
 
-    def move(self, location=None, coord_type=None):
-        self.location = self.arg1 if location is None else location
-        self.coord_type = self.coord_type if coord_type is None else coord_type
+        """
+        self._setvar({'backend': 'xgpu', 'project_id': None}, kwargs)
+        if self.project_id is None:
+            print("Need a project_id")
+            return
+        metadata.onlog(f"Backend {self.backend} for project {self.project_id}")
+        if ata_control is None:
+            print("Test backend")
+        else:
+            if self.backend == 'xgpu':
+                subprocess.run("ansible-playbook /home/sonata/src/ansible_playbooks/hashpipe/xgpu_record.yml")
+            else:
+                print(f"Invalid backend: {self.backend} -- no action")
+                return
+            subprocess.run(f"/home/sonata/src/observing_campaign/backend_setup_scripts/set_keys_uvh5_{self.project_id}.py")
+
+    def move(self, **kwargs):
+        """
+        Parameters (kwargs)
+        -------------------
+        location : str
+            target location (depends on coord_type)
+        coord_type : str
+            coordination type (azel, radec, source, traj)
+        use_ants : list
+            List of antennas to move, default to group_ants
+
+        """
+        self._setvar({'location': None, 'coord_type': 'azel', 'use_ants': None, 'group_ants': ['1a']}, kwargs)
+        if self.location is None:
+            print("Need target")
+            return
 
         if ',' in self.location:
             x, y = [float(_v) for _v in self.location.split(',')]
+        metadata.onlog(f'move to: {self.location}  {self.coord_type}')
         if ata_control is None:
             self.test('test move')
             return
 
-        metadata.onlog(f'move to: {self.coord_type}')
         if self.coord_type == 'azel':
             ata_control.set_az_el(self.use_ants, x, y)
             metadata.onlog(f"azel: {x},{y}")
@@ -98,16 +177,31 @@ class CommandHandler:
             except FileNotFoundError:
                 pass
     
-    def note(self, notation=None):
-        self.notation = self.arg1 if notation is None else notation
+    def note(self, **kwargs):
+        """
+        Parameter (kwargs)
+        ------------------
+        notation : str
+            Note to log
+
+        """
+        self._setvar({'notation': None}, kwargs)
         if self.notation is None:
             print("Need to include a note.")
         else:
             metadata.onlog(f"note: {self.notation}")
 
-    def source(self, name=None, datestamp=None):
-        self.name = self.arg1 if name is None else name
-        self.datestamp = self.arg2 if datestamp is None else datestamp
+    def source(self, **kwargs):
+        """
+        Parameters (kwargs)
+        -------------------
+        source : str
+            Source name
+        datestamp : str
+            Datestamp
+
+        """
+        self._setvar({'name': None, 'datestamp': None}, kwargs)
         self.datestamp = onutil.make_datetime(date=self.datestamp)
         metadata.onlog([f'source: {self.name}', f'expected: {self.datestamp.isoformat()}'])
 
--- scripts/obsnerd.py
+++ scripts/obsnerd.py
@@ -4,11 +4,21 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('cmd', help="Action [start, freq,  move, end, note, source, summary]", choices=['start', 'freq', 'move', 'end', 'note', 'source', 'summary'])
-ap.add_argument('arg1', help="Argument for command.", nargs='?', default=None)
-ap.add_argument('arg2', help="If source, associated datetime (secondary argument).", nargs='?', default=None)
-ap.add_argument('-c', '--coord-type', dest='coord_type', help="For <move> coordinate type: azel/radec/source/traj [azel]",
+ap.add_argument('-i', '--initials', help="Initials of user (cmd=start)", default=None)
+ap.add_argument('-p', '--project_id', help="Project ID designator (cmd=start;backend, default=p054)", default='p054')
+ap.add_argument('--group_ants', help="csv-list of antennas to reserve in group (cmd=start;end, default=1a)", default='1a')
+ap.add_argument('-u', '--use_ants', help="csv-list of antennas to use or None (cmd=move;freq;end, default=<group_ants>)", default=None)
+ap.add_argument('-f', '--frequency', help="Frequency of observation [GHz] (cmd=freq)", default=None)
+ap.add_argument('-a', '--attenuation', help="Attenuation [dB] (cmd=freq, default=20)", type=int, default=20)
+ap.add_argument('--lo', help="LO to set (cmd=freq, default=A)", choices=['A', 'B', 'C', 'D'], default='A')
+ap.add_argument('-b', '--back_end', help="Type of backend (cmd=backend, default=xgpu)", choices=['xgpu'], default='xgpu')
+ap.add_argument('-l', '--location', help="Location for coord_type (cmd=move)", default=None)
+ap.add_argument('-c', '--coord_type', help="For <move> coordinate type: azel/radec/source/traj [azel] (cmd=move, default=azel)",
                 choices=['azel', 'radec', 'source', 'traj'], default='azel')
+ap.add_argument('-n', '--notation', help="Note to add (cmd=note)", default=None)
+ap.add_argument('-s', '--source', help="Source name for log (cmd=source)", default=None)
+ap.add_argument('-d', '--datetime', help="Reference datetime for source (cmd=source)", default=None)
 args = ap.parse_args()
 
-session = oe.CommandHandler(arg1=args.arg1, arg2=args.arg2, coord_type=args.coord_type)
-getattr(session, args.cmd)()
+session = oe.CommandHandler()
+getattr(session, args.cmd)(**vars(args))
