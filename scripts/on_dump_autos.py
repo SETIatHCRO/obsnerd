b8cb0c818144c009caaddaec3e533c15f67f42ec
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-10 15:56:20 -0800

    accommodate extra underscore on next script

--- scripts/on_dump_autos.py
+++ scripts/on_dump_autos.py
@@ -13,9 +13,6 @@ class Dump:
         self.lo = lo
         self.cnode = cnode
         self.source = fn.split('_')[UVH5_SRC_IND]  # Current standard format
-        #if self.source[0] == 'S':
-        print("AD HOC TO HANDLE THE EXTRA UNDERSCORE")
-        self.source = f"{self.source}_{fn.split('_')[UVH5_SRC_IND+1]}"
         self.freq_unit = freq_unit
         self.read_uvh5()
 
