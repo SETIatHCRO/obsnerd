0ff15cf3dce962a708c05de60c3e317b3428334c
DAVID DEBOER <ddeboer@berkeley.edu>
2025-01-25 20:44:05 -0800

    renamed packaged from obsnerds to obsnerd

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from setuptools import setup
 import glob
 
 setup_args = {
-    'name': "obsnerds",
+    'name': "obsnerd",
     'description': "NRDZ Observing for the ATA",
     'license': "BSD",
     'author': "David DeBoer",
     'author_email': "david.r.deboer@gmail.edu",
     'version': '0.2.0',
     'scripts': glob.glob('scripts/*'),
-    'packages': ['obsnerds']
+    'packages': ['obsnerd']
 }
 
 if __name__ == '__main__':
