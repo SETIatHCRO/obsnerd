5ad3eccf19fa5b58340eff5a44e1244cc5484e07
DAVID DEBOER <ddeboer@berkeley.edu>
2025-02-18 21:08:12 -0800

    starting to work with new format

--- scripts/on_look.py
+++ scripts/on_look.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
     ap.add_argument('-w', '--waterfall', help="Flag to generate all of the waterfalls.", action='store_true')
     ap.add_argument('-s', '--save', help="Save the generated plots", action='store_true')
     ap.add_argument('-t', '--time_axis', help="Type of 'time' axis for dashboard ([d]atetime, [s]econds, [b]oresight)", default='boresight')
-    ap.add_argument('--dir_data', help="directory for the observation data", default='./data')
     ap.add_argument('--dash', help="Generate the dashboard", action='store_true')
     ap.add_argument('--show_diff', help="Show the difference from the dashboard", action='store_true')
     ap.add_argument('--cnode', help="If 'source' is an obsid, need cnodes to use.", default='352,544,736,928,1120,1312,1504')
@@ -25,11 +24,11 @@ if __name__ == '__main__':
     args = ap.parse_args()
     args.time_axis = AXIS_OPTIONS[args.time_axis[0].lower()]
 
-    look = onv_look.Look(obsid=args.obsid, lo=args.lo, cnode=args.cnode, freq_unit=args.freq_unit, dir_data=args.dir_data)
+    look = onv_look.Look(oinput=args.obsid, lo=args.lo, cnode=args.cnode, freq_unit=args.freq_unit)
+    look.read_in_files()
     if args.dash:
         for ant in args.ant.split(','):
             for pol in args.pol.split(','):
                 look.dashboard(ant=ant, pol=pol, save=args.save, time_axis=args.time_axis, show_diff=args.show_diff)
-        look.dashboard(ant=ant, pol=pol, save=args.save, time_axis=args.time_axis, show_diff=args.show_diff)
     if not args.save:
         plt.show()
