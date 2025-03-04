0bdeef29b44348a7cd1b8e1519a443d22af9605c
DAVID DEBOER <ddeboer@berkeley.edu>
2025-03-03 21:36:17 -0800

    add copy files to dump and update doc

--- obsnerd/onv_dump.py
+++ obsnerd/onv_dump.py
@@ -35,8 +35,8 @@ def gen_uvh5_dump_script(date_path, base_path='/mnt/primary/ata/projects/p054/',
     with open(cp_script_filename, 'w') as fp:
         for obsrec, data in files.items():
             dsplit = data['filename'].split('/')
-            dir2go = dsplit[8]
-            nfn = op.join(dir2go, dsplit[9])
+            print(dsplit[8], dsplit[9])
+            nfn = op.join(dsplit[8], dsplit[9])
             print(f"cp {data['filename']} {nfn}", file=fp)
  
     with open(script_filename, 'w') as fp:
