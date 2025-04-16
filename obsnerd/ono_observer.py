972cd06b1759f20697d66aaa605358004e2cd502
DAVID DEBOER <ddeboer@berkeley.edu>
2025-04-16 16:52:49 -0700

    reenable sources etc for observing

--- obsnerd/ono_observer.py
+++ obsnerd/ono_observer.py
@@ -182,7 +182,7 @@ class Observer:
             print(f"freqs: {', '.join([str(x) for x in source.freq])}")
             these_freq = [x.to_value('MHz') for x in source.freq]
             self.obs.setrf(freq=these_freq, lo=source.lo, attenuation=source.attenuation)
-            self.obs.move(source=source.source_name, coord_type=source.coord)
+            self.obs.move(source=source.source, coord_type=source.coord)
             tlength = ttools.wait(ttools.t_delta(source.start, -1.0*self.obs.obs_start_delay, 's'))
             if tlength is None:
                 continue
