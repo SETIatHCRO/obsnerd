3b0273073662a90ec01ecee7bcdd48784c1d6a02
DAVID DEBOER <ddeboer@berkeley.edu>
2025-05-29 14:06:48 -0700

    fix observe prep script

--- scripts/on_obs_prep.py
+++ scripts/on_obs_prep.py
@@ -4,7 +4,7 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--add-to-calendar', dest='add_to_calendar', help="Add the observations to the calendar", action='store_true')
-ap.add_argument('--ods2use', help="ODS file to use for observations", default='/opt/mnt/share/ods_project/ods_rados.json')
+ap.add_argument('--ods_rados', help="ODS file to use for ODS publishing", default='/opt/mnt/share/ods_project/ods_rados.json')
 ap.add_argument('--ods-upload', dest='ods_upload', help="ODS file to upload for TBA", default="/opt/mnt/share/ods_upload/ods.json")
 ap.add_argument('--ods-active', dest='ods_active', help="Active ODS location", default="https://www.seti.org/sites/default/files/HCRO/ods.json")
 # Note the inverted logic...
@@ -15,8 +15,7 @@ args = ap.parse_args()
 # Initialize the observer
 observer = ono_observer.Observer(project_name='SatSpot', project_id='p054')
 observer.observe_prep(add_to_calendar=args.add_to_calendar,
-                     ods2use=args.ods2use,
+                     ods_rados=args.ods_rados,
                      ods_upload=args.ods_upload,
-                     ods_active=args.ods_active,
                      update_source_database=args.update_source_database,
                      ods_assembly=args.ods_assembly)
