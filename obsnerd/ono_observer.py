2de6b06a7da43358467bc70e158f6a7b2930b2d2
DAVID DEBOER <ddeboer@berkeley.edu>
2025-09-13 10:13:54 -0700

    fix ods add

--- obsnerd/ono_observer.py
+++ obsnerd/ono_observer.py
@@ -107,7 +107,7 @@ class Observer:
                         pars[par] = entries[i][par]
                     new_entry = copy(entries[i])
                     new_entry.update({'freq_lower_hz': SPACEX_LO.to_value('Hz'), 'freq_upper_hz': SPACEX_HI.to_value('Hz')})
-                    self.ods.new_entry(**new_entry, instance_name="OUTPUT_ALL")
+                    self.ods.new_record(**new_entry, instance_name="OUTPUT_ALL")
                     if 'Make' in entries[i].get('version', 'Make'):  # Only add to OUTPUT_ODS if Make string in version, default to Make
                         self.ods.new_entry(**new_entry, instance_name="OUTPUT_ODS")
                 else:  # Just check if different
